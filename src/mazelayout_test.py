7237840f3a639c82f05b3729b631beda10a09575
Nifrec <lulof.piree@zoho.com>
2020-04-07 17:14:58 +0200

    Add collision test

--- src/mazelayout_test.py
+++ src/mazelayout_test.py
@@ -13,7 +13,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Base case: normal ball hits normal wall.
         """
-        lines = set(Line(0, 3, 3, 0))
+        lines = set([Line(0, 3, 3, 0)])
         ball = Ball(0, 0, 2.5)
         layout = MazeLayout(lines)
 
@@ -23,7 +23,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Base case: normal ball does not hit a wall.
         """
-        lines = set(Line(0, 3, 3, 0))
+        lines = set([Line(0, 3, 3, 0)])
         ball = Ball(0, 0, 1)
         layout = MazeLayout(lines)
 
@@ -33,7 +33,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Corner case: normal ball just hits a wall.
         """
-        lines = set(Line(3, 3, 3, 0))
+        lines = set([Line(3, 3, 3, 0)])
         ball = Ball(0, 1.5, 3)
         layout = MazeLayout(lines)
 
@@ -43,7 +43,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Base case: 2 walls, no hit.
         """
-        lines = set(Line(0, 3, 3, 0), Line(-4, -6, 3, 0))
+        lines = set([Line(0, 3, 3, 0), Line(-4, -6, 3, 0)])
         ball = Ball(5, 6, 1)
         layout = MazeLayout(lines)
 
@@ -53,7 +53,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Base case: 2 walls, one hits.
         """
-        lines = set(Line(0, 3, 3, 0), Line(-4, -6, 3, 0))
+        lines = set([Line(0, 3, 3, 0), Line(-4, -6, 3, 0)])
         ball = Ball(-4, -5, 2)
         layout = MazeLayout(lines)
 
@@ -63,7 +63,7 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Corner case: endpoints of line are far away.
         """
-        lines = set(Line(-5, -5, 5, 5))
+        lines = set([Line(-5, -5, 5, 5)])
         ball = Ball(0, 0, 1)
         layout = MazeLayout(lines)
 
@@ -73,12 +73,83 @@ class MazeLayoutTestCase(unittest.TestCase):
         """
         Corner case: Does not hit but would do so if the line were longer.
         """
-        lines = set(Line(-5, -5, -3, -3))
+        lines = set([Line(-5, -5, -3, -3)])
         ball = Ball(0, 0, 1)
         layout = MazeLayout(lines)
 
         assert (layout.get_ball_hits_wall(ball) == False)
 
+    def test_get_ball_hits_wall_8(self):
+        """
+        Corner case: Does not hit but would do so if the line were longer v2.
+        """
+        lines = set([Line(-5, 5, 0, 0)])
+        ball = Ball(5, -5, 1)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == False)
+
+    def test_get_ball_hits_wall_9(self):
+        """
+        Corner case: Does not hit but would do so if the line were longer v3.
+        """
+        lines = set([Line(1, 5, 1, 4)])
+        ball = Ball(1, 0, 1)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == False)
+
+    def test_get_ball_hits_wall_10(self):
+        """
+        Corner case: Does not hit but would do so if the line were longer v4.
+        """
+        lines = set([Line(1, 10, -5, 10)])
+        ball = Ball(6, 10, 2)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == False)
+
+    def test_get_ball_hits_wall_11(self):
+        """
+        Corner case: Hits, both points of line left of ball center.
+        """
+        lines = set([Line(0, 100, 0, -100)])
+        ball = Ball(1, 0, 3)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == True)
+
+    def test_get_ball_hits_wall_12(self):
+        """
+        Corner case: Hits, both points of line right of ball center.
+        """
+        lines = set([Line(0, 100, 0, -100)])
+        ball = Ball(-1, 0, 3)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == True)
+
+    def test_get_ball_hits_wall_13(self):
+        """
+        Corner case: coordinates line points all smaller than
+        ball coordinate values (of respective dims).
+        """
+        lines = set([Line(0, -3, -3, 0)])
+        ball = Ball(0, 0, 2.5)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == True)
+
+    def test_get_ball_hits_wall_14(self):
+        """
+        Corner case: Does not hit but would do so if the line were longer v5:
+        One point bigger y and one point smaller y than ball.
+        """
+        lines = set([Line(-100, -0.1, -5, 0.1)])
+        ball = Ball(0, 0, 4)
+        layout = MazeLayout(lines)
+
+        assert (layout.get_ball_hits_wall(ball) == False)
 
 if (__name__ == "__main__"):
     unittest.main()
--- src/model.py
+++ src/model.py
@@ -4,7 +4,14 @@ Model providing the maze-simulation of the environment.
 Author: Lulof Pirée
 """
 import math
+import numpy as np
+from numpy import linalg as LA
 
+def dot(p1, p2):
+    """
+    Returns dot product of two points.
+    """
+    return p1.x*p2.x + p1.y*p2.y
 
 class Size():
     """
@@ -36,20 +43,11 @@ class Line():
     """
 
     def __init__(self, x1, y1, x2, y2):
-        self.p1 = Point(x1, y1)
-        self.p2 = Point(x2, y2)
+        self.p1 = np.array([x1, y1])
+        self.p2 = np.array([x2, y2])
         #self.length = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
         # Coefficients of equation ax + by + c = 0 for this line.
-        # Proof:
-        # [x2]   [x1-x2]          [x]
-        # [y2] + [y1-y2]*lambda = [y]
-        # 
-        # --> lambda = (x - x2)/(x1-x2)
-        # --> y = y2 + (y1-y2)*lambda = y2 + (y1-y2)(x - x2)/(x1-x2)
-        # --> y(x1 - x2) + x(y2 - y1) + y2(x2 - x1) +x2(y1 - y2) = 0
-        #self.a = y2 - y1
-        #self.b = x1 - x2
-        #self.c = y2*(x2 - x1) + x2*(y1 - y2)
+        
 
     def __str__(self):
         return f"Line ({self.p1.x}, {self.p1.y}) -> ({self.p2.x}, {self.p2.y})"
@@ -60,8 +58,7 @@ class Ball():
     velocity (in 2 dimensions) and acceleration (in 2 dimensions).
     """
     def __init__(self, x, y, rad, x_vel=0, y_vel=0, x_acc=0, y_acc=0):
-        self.x = x
-        self.y = y
+        self.p = np.array([x, y])
         self.rad = rad
         self.x_vel = x_vel
         self.y_vel = y_vel
@@ -88,19 +85,71 @@ class MazeLayout():
         Note that just hitting without overlapping counts as hitting.
         Also note that lines have a finite length here.
         """
-        # First check if the distance of the center of the ball to the line
-        # is smaller than the radius of the ball.
-        # Proof: https://brilliant.org/wiki/dot-product-distance-between-point-and-a-line/
-        distance = abs(line.a * ball.x + line.b * ball.y + line.c ) \
-                / math.sqrt(line.a**2 + line.b**2)
-        if (((line.x1 < ball.x) == (line.x2 < ball.x)) and
-                ((line.y1 < ball.y) == (line.y2 < ball.y))):
-                return False
+        distance = dist_point_segment_line(ball.p, line)
         
         if (distance <= ball.rad):
             return True
-        return False
+        else:
+            return False
 
 
-        # 
+def get_line_implicit_coefs(line):
+    """
+    Returns the coeficients of the implicit 2D equation
+    of a line.
+    That it, it returns a, b, c in :
+            ax + by + c = 0
+    """
+    # Proof:
+    # [x2]   [x1-x2]          [x]
+    # [y2] + [y1-y2]*lambda = [y]
+    # 
+    # --> lambda = (x - x2)/(x1-x2)
+    # --> y = y2 + (y1-y2)*lambda = y2 + (y1-y2)(x - x2)/(x1-x2)
+    # --> y(x1 - x2) + x(y2 - y1) + y2(x2 - x1) +x2(y1 - y2) = 0
+    x1 = line.p1[0]
+    y1 = line.p1[1]
+    x2 = line.p2[0]
+    y2 = line.p2[1]
+    
+    a = y2 - y1
+    b = x1 - x2
+    c = y2*(x2 - x1) + x2*(y1 - y2)
 
+    return a, b, c
+
+def dist_point_segment_line(point, line):
+    """
+    Get the distance of a Point to a Line,
+    treating the Line as a finite line segment.
+    """
+    # Derivation: http://geomalgorithms.com/a02-_lines.html 
+    v = line.p2 - line.p1
+    w = point - line.p1
+
+    # Check if point beyond p1-end of line
+    c = np.dot(v, w)
+    if (c <= 0):
+        return euclidean_dist(point, line.p1)
+    # Check if point beyond p2-end of line
+    elif (np.dot(v, v) <= c):
+        return euclidean_dist(point, line.p2)
+    # Then return the distance to the (inf) line itself.
+    return dist_point_inf_line(point, line)
+
+def euclidean_dist(p1, p2):
+    """
+    Returns Euclidean distance between two points.
+    """
+    return LA.norm(p1 - p2)
+
+def dist_point_inf_line(point, line):
+    """
+    Get the distance of a Point to a Line,
+    treating the Line as an infinite line.
+    """
+    # Proof: https://brilliant.org/wiki/dot-product-distance-between-point-and-a-line/
+    a, b, c = get_line_implicit_coefs(line)
+    distance = abs(a * point[0] + b * point[1] + c ) \
+                / math.sqrt(a**2 + b**2)
+    return distance
