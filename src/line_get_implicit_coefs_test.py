ddc93285e87de524de2705780e0715cf370565df
Nifrec <lulof.piree@zoho.com>
2020-04-07 17:14:46 +0200

    Rename testcases

--- src/line_get_implicit_coefs_test.py
+++ src/line_get_implicit_coefs_test.py
@@ -4,11 +4,11 @@ File to test the Line class of model.py
 Author: Lulof Pirée
 """
 import unittest
-from model import Line
+from model import Line, get_line_implicit_coefs
 
-class LineTestCase(unittest.TestCase):
+class get_line_implicit_coefs_test(unittest.TestCase):
 
-    def test_abc1(self):
+    def test_line_implicit_coefs_1(self):
         """
         Base case for the equation-coefficients computation.
         """
@@ -17,12 +17,12 @@ class LineTestCase(unittest.TestCase):
         ex_a = 1
         ex_b = -1
         ex_c = 0
+        a, b, c = get_line_implicit_coefs(line)
+        assert(ex_a == a), f"a: expected: {ex_a}, got: {a}"
+        assert(ex_b == b)
+        assert(ex_c == c)
 
-        assert(ex_a == line.a), f"a: expected: {ex_a}, got: {line.a}"
-        assert(ex_b == line.b)
-        assert(ex_c == line.c)
-
-    def test_abc2(self):
+    def test_line_implicit_coefs_2(self):
         """
         Base case for the equation-coefficients computation.
         """
@@ -31,10 +31,10 @@ class LineTestCase(unittest.TestCase):
         ex_a = -4 #x multiplier
         ex_b = -2 #y multiplier
         ex_c = 8
-
-        assert(ex_a == line.a), f"a: expected: {ex_a}, got: {line.a}"
-        assert(ex_b == line.b), f"b: expected: {ex_b}, got: {line.b}"
-        assert(ex_c == line.c)
+        a, b, c = get_line_implicit_coefs(line)
+        assert(ex_a == a), f"a: expected: {ex_a}, got: {a}"
+        assert(ex_b == b), f"b: expected: {ex_b}, got: {b}"
+        assert(ex_c == c)
 
 if (__name__ == "__main__"):
     unittest.main()
