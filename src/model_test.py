72390eb14945acc7b0d3a1e0a753d61f147bd9bf
Nifrec <lulof.piree@zoho.com>
2020-04-08 16:42:47 +0200

    Bugfixes & output tweaks

--- src/model_test.py
+++ src/model_test.py
@@ -11,12 +11,16 @@ class ModelMovementTestCase(unittest.TestCase):
 
     def setUp(self):
         self.size = Size(10000, 10000)
-        self.ball_size = 1
+        self.ball_rad = 1
         self.start = np.array([50, 50])
         self.layout = MazeLayout(set([]), self.start, 
                 np.array([1000, 1000]), self.size)
-        self.model = Model(self.size, self.ball_size)
+        self.model = Model(self.size, self.ball_rad)
         self.model.reset(self.layout)
+
+    def compare_pos(self, expected, result):
+        assert np.allclose(expected, result), \
+            f"expected:{expected}, result:{result}"
 
     def test_movement_1(self):
         """
@@ -26,12 +30,12 @@ class ModelMovementTestCase(unittest.TestCase):
         self.model.make_timestep()
         # Velocity should now be [1, 0]
         self.model.set_acceleration(0, 0)
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([1, 0]))
+        self.compare_pos(self.start + np.array([1, 0]),
+                self.model.get_ball_position())
         self.model.make_timestep()
         # Velocity should remain be [1, 0]
-        assert np.allclose(self.model.get_ball_position,
-                self.start + 2*np.array([1, 0]))
+        self.compare_pos(self.start + 2*np.array([1, 0]),
+                self.model.get_ball_position())
 
     def test_movement_2(self):
         """
@@ -41,14 +45,18 @@ class ModelMovementTestCase(unittest.TestCase):
         self.model.make_timestep()
         # Velocity should now be [0, 2]
         
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([0, 2]))
+        self.compare_pos(
+                self.start + np.array([0, 2]),
+                self.model.get_ball_position()
+                )
 
         self.model.set_acceleration(0, 0)
         self.model.make_timestep()
         # Velocity should remain be [1, 0]
-        assert np.allclose(self.model.get_ball_position,
-                self.start + 2*np.array([0, 2]))
+        self.compare_pos(
+                self.start + 2*np.array([0, 2]),
+                self.model.get_ball_position()
+                )
 
     def test_movement_3(self):
         """
@@ -57,17 +65,24 @@ class ModelMovementTestCase(unittest.TestCase):
         self.model.set_acceleration(1, 1)
         self.model.make_timestep()
         # Velocity should now be [1, 1]
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([1, 1]))
+        self.compare_pos(
+                self.start + np.array([1, 1]),
+                self.model.get_ball_position()
+                )
 
         self.model.set_acceleration(-2, -2)
         self.model.make_timestep()
         # Velocity should be [-1, -1]
-        assert np.allclose(self.model.get_ball_position,
-                self.start)
+        self.compare_pos(
+                self.start,
+                self.model.get_ball_position()
+                )
+        self.model.set_acceleration(0, 0)
         self.model.make_timestep()
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([-1, -1]))
+        self.compare_pos(
+                self.start + np.array([-1, -1]),
+                self.model.get_ball_position()
+                )
 
     def test_movement_4(self):
         """
@@ -76,17 +91,23 @@ class ModelMovementTestCase(unittest.TestCase):
         self.model.set_acceleration(1, 1)
         self.model.make_timestep()
         # Velocity should now be [1, 1]
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([1, 1]))
+        self.compare_pos(
+                self.start + np.array([1, 1]),
+                self.model.get_ball_position()
+                )
 
         self.model.make_timestep()
         # Velocity should be [2, 2]
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([3, 3]))
+        self.compare_pos(
+                self.start + np.array([3, 3]),
+                self.model.get_ball_position()
+                )
         # Velocity should be [3, 3]
         self.model.make_timestep()
-        assert np.allclose(self.model.get_ball_position,
-                self.start + np.array([6, 6]))
+        self.compare_pos(
+                self.start + np.array([6, 6]),
+                self.model.get_ball_position()
+                )
 
 if (__name__ == "__main__"):
     unittest.main()
