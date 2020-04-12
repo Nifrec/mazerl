221023454b93bc33052ecca3539bf4ef8893ff86
Nifrec <lulof.piree@zoho.com>
2020-04-12 15:36:38 +0200

    Store expected walls in fields

--- src/maze_generator_test.py
+++ src/maze_generator_test.py
@@ -16,30 +16,37 @@ class MazeBlockTestCase(unittest.TestCase):
 
     def setUp(self):
         self.block = MazeBlock(10, 10, 15, 15)
+        self.left_wall = Line(10, 10, 10, 25)
+        self.right_wall = Line(25, 10, 25, 25)
+        self.top_wall = Line(10, 10, 25, 10)
+        self.bottom_wall =  Line(10, 25, 25, 25)
 
     def test_left_wall(self):
         wall = self.block.generate_wall(Direction.LEFT)
-        expected = Line(10, 10, 10, 25)
+        expected = self.left_wall
         self.assertTrue(np.allclose(wall.p0, expected.p0))
         self.assertTrue(np.allclose(wall.p1, expected.p1))
 
     def test_right_wall(self):
         wall = self.block.generate_wall(Direction.RIGHT)
-        expected = Line(25, 10, 25, 25)
+        expected = self.right_wall
         self.assertTrue(np.allclose(wall.p0, expected.p0))
         self.assertTrue(np.allclose(wall.p1, expected.p1))
 
     def test_top_wall(self):
         wall = self.block.generate_wall(Direction.UP)
-        expected = Line(10, 10, 25, 10)
+        expected = self.top_wall
         self.assertTrue(np.allclose(wall.p0, expected.p0))
         self.assertTrue(np.allclose(wall.p1, expected.p1))
 
     def test_bottom_wall(self):
         wall = self.block.generate_wall(Direction.DOWN)
-        expected = Line(10, 25, 25, 25)
+        expected = self.bottom_wall
         self.assertTrue(np.allclose(wall.p0, expected.p0))
         self.assertTrue(np.allclose(wall.p1, expected.p1))
+
+    def test_walls_with_path(self):
+        pass
 
 if (__name__ == "__main__"):
     unittest.main()
