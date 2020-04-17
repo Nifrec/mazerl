cbf74e2dc5da23cee66c12bbeb8f1af9abced2eb
Nifrec <lulof.piree@zoho.com>
2020-04-17 14:37:09 +0200

    Add command-line interface

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Main file giving a single access point to the project:
 from this file the human interface can be launched 
@@ -7,6 +8,35 @@ or load a trained agent.
 
 Author: Lulof Pirée
 """
-from maze.human_interface import HumanInterface
+import argparse
+import os
+import settings
 
-HumanInterface()
+def start_training():
+    path_to_main = os.path.dirname(__file__)
+    checkpoint_dir = os.path.join(path_to_main, settings.CHECKPOINT_TOP_DIR_NAME)
+    if not os.path.exists(checkpoint_dir):
+        print("Creating checkpint folder:\n" + checkpoint_dir)
+        os.mkdir(checkpoint_dir)
+
+COMMANDS = ("human", "train", "load")
+parser = argparse.ArgumentParser()
+parser.add_argument("command", choices=COMMANDS, 
+        help="Function of the program to launch")
+parser.add_argument("-d", "--checkpoint-directory", required=False, 
+        help="path to folder of saved agent (in case relevant)",
+        metavar="path", dest="checkpoint_dir")
+args = parser.parse_args()
+
+if (args.command == "human"):
+    from maze.human_interface import HumanInterface
+    HumanInterface()
+elif (args.command == "train"):
+    print("Sorry, this feature is still WIP")
+    start_training()
+elif (args.command == "load"):
+    print(args.checkpoint_dir)
+    print("Sorry, this feature is still WIP")
+
+
+
---
+++ src/settings.py
@@ -0,0 +1,7 @@
+"""
+Various settings for the program launcher and training hyperparameters.
+
+Author: Lulof Pirée
+"""
+# Parent directory of all checkpoint directories:
+CHECKPOINT_TOP_DIR_NAME = "checkpoints"
