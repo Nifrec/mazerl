55fed37a35a689b48e7c42b51f9c8f11463b0c9b
Nifrec <lulof.piree@zoho.com>
2020-04-18 11:28:30 +0200

    Minor fixes and type hints

--- src/agent/trainer.py
+++ src/agent/trainer.py
@@ -38,7 +38,8 @@ class Trainer:
     Class for training, saving and executing a DDPG or TD3 agent.
     """
 
-    def __init__(self, hyperparameters, agent: Agent, logger: Logger):
+    def __init__(self, hyperparameters: HyperparameterTuple,
+            agent: Agent, logger: Logger):
         """
         Initialize the DDPG training algorithm, set up Gym environment,
         create file with setup info.
@@ -50,50 +51,48 @@ class Trainer:
         * env_name: string, name of environment known by gym.
         * mode: string, "DDPG" or "TD3"
         """
-        self.hyperparameters = hyperparameters
-        self.env = hyperparameters.env
-        self.mode = hyperparameters.mode
-        self.state_size = len(self.env.reset())
+        self.__hyperparameters = hyperparameters
+        self.__env = hyperparameters.env
         self.__agent = agent
         self.__logger = logger
 
         # Prefer CUDA (=GPU), which is normally faster.
-        self.device = \
+        self.__device = \
                 torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def train(self):
 
-        replay_memory = ReplayMemory(self.hyperparameters.memory_capacity)
+        replay_memory = ReplayMemory(self.__hyperparameters.memory_capacity)
         rewards_per_episode = [] # For plot
         self.critic_losses = []
         self.actor_losses = []
 
-        for episode in range(1, self.hyperparameters.num_episodes+1):
-            state = self.env.reset()
+        for episode in range(1, self.__hyperparameters.num_episodes+1):
+            state = self.__env.reset()
             episode_rewards = []
             # The neural networks only accept tensors as input.
-            state = torch.tensor([state], dtype=torch.float).to(self.device)
+            state = torch.tensor([state], dtype=torch.float).to(self.__device)
             
-            for timestep in range(self.hyperparameters.max_episode_duration):
+            for timestep in range(self.__hyperparameters.max_episode_duration):
                 action = self.__choose_action(state, episode)
 
-                next_state, reward, done, _ = self.env.step(
+                next_state, reward, done, _ = self.__env.step(
                         action.tolist()[0])
                 episode_rewards += [reward]
                 # Store experience as 4 tensors, since used in neural networks.
                 next_state = torch.tensor([next_state],
-                        dtype=torch.float).to(self.device)
+                        dtype=torch.float).to(self.__device)
                 reward = torch.tensor([reward], dtype=torch.float)\
-                        .to(self.device)
+                        .to(self.__device)
                 done = torch.tensor([done], dtype=torch.bool)\
-                        .to(self.device)
+                        .to(self.__device)
                 new_experience = Experience(state, action, reward, next_state,
                         done)
                 replay_memory.push(new_experience)
 
-                if (replay_memory.can_sample(self.hyperparameters.batch_size)):
+                if (replay_memory.can_sample(self.__hyperparameters.batch_size)):
                     batch = replay_memory.sample(
-                            self.hyperparameters.batch_size)
+                            self.__hyperparameters.batch_size)
                     self.__update_agent(episode, batch)
 
                 if (done.item()):
@@ -108,25 +107,26 @@ class Trainer:
 
         self.__logger.close_files()
 
-    def __choose_action(self, state, episode):
+    def __choose_action(self, state: torch.Tensor, episode: int) 
+            -> Iterable[Number]:
         """
         Lets agent choose either a random action at the beginning
         or an action according to its inference actor.
         """
-        select_random = (episode < self.hyperparameters.random_action_episodes)
-        action, value_estimation = self.agent.choose_action(state,
+        select_random = \
+                (episode < self.__hyperparameters.random_action_episodes)
+        action, value_estimation = self.__agent.choose_action(state,
                 select_random)
 
         if (self.logger is not None):
             self.logger.push_value_estimations(value_estimation)
             if (self.logger.has_trained_critic()):
                 self.logger.push_trained_critic_experience(
-                    state.to(self.device), action)
-
+                        state.to(self.__device), action)
         return action
 
-    def __update_agent(self, episode, batch):
-        critic_loss, actor_loss = self.agent.update(batch, episode)
+    def __update_agent(self, episode: int, batch: Tuple[Experience]):
+        critic_loss, actor_loss = self.__agent.update(batch, episode)
         if self.logger is not None:
             self.logger.push_critic_loss(critic_loss)
             if actor_loss is not None:
@@ -139,37 +139,37 @@ class Trainer:
         if (self.logger is not None):
             self.logger.push_rewards(episode_rewards)
 
-        if (episode % self.hyperparameters.plot_interval == 0):
+        if (episode % self.__hyperparameters.plot_interval == 0):
             self.__plot_and_print(episode, rewards_per_episode)
 
-        if (episode % self.hyperparameters.checkpoint_interval == 0):
+        if (episode % self.__hyperparameters.checkpoint_interval == 0):
             self.__save_progress(episode, rewards_per_episode)
             self.logger.update()
 
     def __plot_and_print(self, episode, rewards_per_episode):
         plot_reward_and_moving_average(
-                        self.hyperparameters.moving_average_period,
+                        self.__hyperparameters.moving_average_period,
                         rewards_per_episode)
         self.__print_train_progress(episode, rewards_per_episode)
 
     def __print_train_progress(self, episode, rewards_per_episode):
-        period = self.hyperparameters.plot_interval//10
+        period = self.__hyperparameters.plot_interval//10
         print(f"Finished episode {episode}")
         print(f"Rewards last {period} episodes:", rewards_per_episode[-period:])
 
     def __save_progress(self, episode, rewards_per_episode):
-        self.agent.save_checkpoint()
+        self.__agent.save_checkpoint()
         plot_reward_and_moving_average(
-                self.hyperparameters.moving_average_period,
+                self.__hyperparameters.moving_average_period,
                 rewards_per_episode, 
-                self.hyperparameters.save_dir
+                self.__hyperparameters.save_dir
         )
 
     def load_checkpoint(self, checkpoint_dir = None):
         if (checkpoint_dir == None):
-            checkpoint_dir = self.hyperparameters.save_dir
+            checkpoint_dir = self.__hyperparameters.save_dir
         
-        self.agent.load_checkpoint(checkpoint_dir)
+        self.__agent.load_checkpoint(checkpoint_dir)
 
     def show_demo(self, max_episode_duration):
         """
@@ -181,21 +181,21 @@ class Trainer:
         * max_episode_duration: int, maximum amount of timesteps before
                 episode will be stopped.
         """
-        state = self.env.reset()
-        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        state = self.__env.reset()
+        state = torch.tensor([state], dtype=torch.float).to(self.__device)
         
         for timestep in range(max_episode_duration):
-            self.env.render()
-            action, _ = self.agent.choose_action(state,
-                    self.hyperparameters.exploration_noise_std)
-            next_state, _, done, _ = self.env.step(action.tolist()[0])
+            self.__env.render()
+            action, _ = self.__agent.choose_action(state,
+                    self.__hyperparameters.exploration_noise_std)
+            next_state, _, done, _ = self.__env.step(action.tolist()[0])
 
             if (done):
                 return
             else:
                 next_state = torch.tensor([next_state], 
-                        dtype=torch.float).to(self.device)
+                        dtype=torch.float).to(self.__device)
                 state = next_state
         # Also render final state after last action
-        self.env.render()
+        self.__env.render()
         print("Demo completed")
