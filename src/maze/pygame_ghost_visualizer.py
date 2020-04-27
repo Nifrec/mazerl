85aae931b22109b226426adf1e7780477c189b89
Nifrec <lulof.piree@zoho.com>
2020-04-27 22:12:59 +0200

    Add dirty rects to ghost -- WIP: fix distortions

--- src/maze/pygame_ghost_visualizer.py
+++ src/maze/pygame_ghost_visualizer.py
@@ -19,34 +19,36 @@ class GhostVisualizer(PygameVisualizer):
 
     def __init__(self, model: Model):
         super().__init__(model)
-        self.__prev_ball = None
+        self.__is_first_render = True
 
     def render(self, screen: pygame.Surface):
         super().render(screen)
 
-    def render_ball(self, ball: Ball, target: pygame.Surface) \
-            -> pygame.Surface:
+    def _draw_below_ball_effects_hook(self, screen: pygame.Surface):
         """
-        Draws a Ball to a pygame.Surface instance,
-        with PygameVisualizer.BALL_COLOR as color.
-        Also draws a transparent 'ghost ball' that lags behind 
-        the location of the ball.
-
-        NOTE: not static like in PygameVisualizer because it has internal state.
-        (i.e. the previous location of the ball.)
+        Adds cleaning up of ghost-ball dirty rect on top
+        of PygameVisualizer's update()
         """
-        if (self.__prev_ball == None):
+        ball_pos = self._model.get_ball_position()
+        ball_rad = self._model.get_ball_rad()
+        if ( self.__is_first_render):
             # Velocity and acceleration are not needed, only the size 
             # and position.
-            self.__prev_ball = Ball(ball.pos[0], ball.pos[1], ball.rad)
+            self.__ghost_ball = Ball(ball_pos[0], ball_pos[1], ball_rad)
+            self.__ghost_ball.pos = self.__ghost_ball.pos.round().astype(np.int) - 1
+            self.__is_first_render = False
 
-        pygame.draw.circle(target, GhostVisualizer.GHOST_COLOR,
-                self.__prev_ball.pos.round().astype(np.int),
-                self.__prev_ball.rad, 0)
-        self.__prev_ball.pos = GhostVisualizer.GHOST_MOVEMENT_DELAY \
-            * self.__prev_ball.pos \
-            + GhostVisualizer.GHOST_MOVEMENT_DELAY * ball.pos.copy()
+        dirty_rect_size = (2*self.__ghost_ball.rad, 2*self.__ghost_ball.rad)
+        dirty_rect = pygame.Rect(self.__ghost_ball.pos, dirty_rect_size)
+        screen.blit(self._layout_surf, 
+                self.__ghost_ball.pos - self.__ghost_ball.rad,
+                dirty_rect)
 
-        target = super().render_ball(ball, target)
+        self.__ghost_ball.pos = GhostVisualizer.GHOST_MOVEMENT_DELAY \
+            * self.__ghost_ball.pos \
+            + GhostVisualizer.GHOST_MOVEMENT_DELAY * ball_pos
+        self.__ghost_ball.pos = self.__ghost_ball.pos.round().astype(np.int)
         
-        return target
+        pygame.draw.circle(screen, GhostVisualizer.GHOST_COLOR,
+                self.__ghost_ball.pos,
+                self.__ghost_ball.rad, 0)
--- src/maze/pygame_visualizer.py
+++ src/maze/pygame_visualizer.py
@@ -31,7 +31,7 @@ class PygameVisualizer(Visualizer):
     LINE_WIDTH = 1
 
     def __init__(self, model: Model):
-        self.__model = model
+        self._model = model
         self.__is_first_render_call = True
 
     def render(self, screen: pygame.Surface):
@@ -54,8 +54,8 @@ class PygameVisualizer(Visualizer):
         on the display in-between render() / update() calls.
         """
         self.__old_ball_pos: np.ndarray
-        ball_pos = self.__model.get_ball_position() # Returns np.ndarray
-        ball_rad = self.__model.get_ball_rad()
+        ball_pos = self._model.get_ball_position() # Returns np.ndarray
+        ball_rad = self._model.get_ball_rad()
         if (self.__is_first_render_call):
             self.__setup_basic_layout(screen)
         else:
@@ -63,14 +63,23 @@ class PygameVisualizer(Visualizer):
 
         # Pos of right-upper corner of rectangle surrounding the ball.
         self.__old_ball_pos =  (ball_pos - ball_rad).round().astype(np.int)
+
+        self._draw_below_ball_effects_hook(screen)
         
-        PygameVisualizer.render_ball(ball_rad, ball_pos, screen)
+        self.render_ball(ball_rad, ball_pos, screen)
+
+    def _draw_below_ball_effects_hook(self, screen: pygame.Surface):
+        """
+        Can be used by subclasses to draw effects inbetween the cleaning
+        of dirty rects and the drawing of the ball.
+        """
+        pass
 
     def __clean_dirty_rect_old_pos_ball(self, screen:pygame.Surface, 
             ball_rad: int):
         dirty_rect_size = (2*ball_rad, 2*ball_rad)
         dirty_rect = pygame.Rect(self.__old_ball_pos, dirty_rect_size)
-        screen.blit(self.__layout_surf, self.__old_ball_pos, dirty_rect)
+        screen.blit(self._layout_surf, self.__old_ball_pos, dirty_rect)
 
     def __setup_basic_layout(self, screen:pygame.Surface):
         """
@@ -79,7 +88,7 @@ class PygameVisualizer(Visualizer):
         moving ball.
         """
         self.__is_first_render_call = False
-        self.__layout_surf = self.__render_maze_layout(screen)
+        self._layout_surf = self.__render_maze_layout(screen)
     
     def __render_maze_layout(self, screen: pygame.Surface) -> pygame.Surface:
         """
@@ -88,9 +97,9 @@ class PygameVisualizer(Visualizer):
         """
         output = pygame.Surface(screen.get_size())
         output.fill(self.BACKGROUND_COLOR)
-        PygameVisualizer.render_end(self.__model.get_layout().get_end(),
+        PygameVisualizer.render_end(self._model.get_layout().get_end(),
                 output)
-        PygameVisualizer.render_lines(self.__model.get_layout().get_lines(),
+        PygameVisualizer.render_lines(self._model.get_layout().get_lines(),
                 output)
         screen.blit(output, (0, 0))
         return output
